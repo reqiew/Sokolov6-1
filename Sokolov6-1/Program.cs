f7ad187a5390110fd3ab9829eb79a51a8244d8e9
dobry <dobry@DESKTOP-FBD3PAQ>
2023-12-03 18:32:07 +0300

    Добавьте файлы проекта.

---
+++ Sokolov6-1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите кол-во чисел для рассчета дисперсии");
+        int n = int.Parse(Console.ReadLine());
+        double[] data = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Введите число");
+            data[i] = double.Parse(Console.ReadLine());
+
+
+        }
+        double mean = sumall(data);
+
+
+        double variance = vr(data, mean);
+
+        Console.WriteLine($"Дисперсия: {variance:F2}");
+    }
+
+    static double sumall(double[] data)
+    {
+        double sum = 0;
+        foreach (double value in data)
+        {
+            sum += value;
+        }
+        double mean = sum / data.Length;
+
+        return mean;
+    }
+
+    static double vr(double[] data, double mean)
+    {
+        double sum = 0;
+        foreach (double value in data)
+        {
+            double d = value - mean;
+            double ds = d * d;
+            sum += ds;
+        }
+        double variance = sum / data.Length;
+
+        return variance;
+    }
+}
